592115658a794c48323380f9ab40f6106057a973
Moira <moira.walker@live.nl>
2021-02-12 17:11:47 +0100

    deleted post controller and service

--- src/main/java/nl/walker/novi/thinkbox/repository/PostRepository.java
+++ src/main/java/nl/walker/novi/thinkbox/repository/PostRepository.java
@@ -1,18 +1,18 @@
-package nl.walker.novi.thinkbox.repository;
-
-import nl.walker.novi.thinkbox.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-/**
- * De JpaRepositories die jullie al kennen. Hier wordt gebruik gemaakt van Query Creation. Op basis van de methode naam
- * weten Spring en JPA welke query op de database uitgevoerd moet worden.
- * Hier kan meer informatie over gevonden worden:
- * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
- */
-
-//NoRepositoryBean
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
+//package nl.walker.novi.thinkbox.repository;
+//
+//import nl.walker.novi.thinkbox.domain.Post;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+///**
+// * De JpaRepositories die jullie al kennen. Hier wordt gebruik gemaakt van Query Creation. Op basis van de methode naam
+// * weten Spring en JPA welke query op de database uitgevoerd moet worden.
+// * Hier kan meer informatie over gevonden worden:
+// * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+// */
+//
+////NoRepositoryBean
+//public interface PostRepository extends JpaRepository<Post, Long> {
+//
+//}
